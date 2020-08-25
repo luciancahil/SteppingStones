a080697eeb3c9a81a10ef1e465179a40c035979e
Roy He <royhe62@yahoo.ca>
2020-08-24 21:09:07 -0700

    Added ability to read radio inputs

--- Question Scripts.js
+++ Question Scripts.js
@@ -23,3 +23,36 @@ function makeInvsible(){
     let index = makeDissapearList.indexOf(this);
     hiddenQsList[index].classList.add("hidden");
 }
+
+
+//The following is used to calculate what services are appropriate
+
+//Stores the Strings that make up the suggestions
+var suggestionsList = [];                             //stores the suggestions
+
+const idS = "You need help with id";                //if they need help with id
+const firstIDS = "You need one piece of ID";        //if they need help getting their first piece of id
+const welfareS = "You need welfare";                //if they need help applying for welfare
+const addictionS = "You need addictions help";      //if they need addictions help
+const mentalS = "You need mentla help";             //if they need mental help
+const diagnoseS = "You need an official diagnosis"; //If they don't have an offical diagnosis yet
+const jobS = "You need help finding a job";         //If they need help finding a job
+const homeS = "You need help finding a home";       //If they need help finding a home
+
+
+suggestionsList[0] = idS; suggestionsList[1] = firstIDS; suggestionsList[2] = welfareS; suggestionsList[3] = addictionS; suggestionsList[4] = mentalS; suggestionsList[5] = diagnoseS; suggestionsList[6] = jobS; suggestionsList[7] = homeS;
+
+
+
+
+//Calculates which strings should be added
+
+var helpSelected = document.getElementsByClassName("help");         //All radio options that indicate some help is needed
+var suggestions = "";           //String that stores the suggestions
+
+function getSuggestions(){
+    for(let i = 0; i < helpSelected.length; i++){
+        if(helpSelected[i].checked)
+            alert(suggestionsList[i]);
+    }
+}
